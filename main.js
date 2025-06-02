b3e7ddbc11dd9cd8b1069e2a19f1e2c0913b452a
moskensoap <141073988+moskensoap@users.noreply.github.com>
2025-06-02 17:50:03 +0000

    fix: prevent window size flicker on startup by delaying show until maximized

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ const {
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1600,
-    height: 1000
+    height: 1000,
+    show: false
   });
   win.maximize();
   win.loadFile('index.html');
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 app.whenReady().then(() => {
